da25094ee9baa5437a786c8ab2612ca03ea12bbf
Peter Greth <peter.greth@gmx.de>
2016-11-01 14:37:13 +0100

    - Replaced static with inline for compatibility with VS 2015 - Added code skeleton for mass spring system - Set the mass spring system as default project to load

---
+++ MassSpringSystem/Simulations/MassSpringSystemSimulator.cpp
@@ -0,0 +1,93 @@
+#include "MassSpringSystemSimulator.h"
+
+MassSpringSystemSimulator::MassSpringSystemSimulator()
+{
+}
+
+// Functions
+const char * MassSpringSystemSimulator::getTestCasesStr()
+{
+	return nullptr;
+}
+
+void MassSpringSystemSimulator::initUI(DrawingUtilitiesClass * DUC)
+{
+}
+
+void MassSpringSystemSimulator::reset()
+{
+}
+
+void MassSpringSystemSimulator::drawFrame(ID3D11DeviceContext * pd3dImmediateContext)
+{
+}
+
+void MassSpringSystemSimulator::notifyCaseChanged(int testCase)
+{
+}
+
+void MassSpringSystemSimulator::externalForcesCalculations(float timeElapsed)
+{
+}
+
+void MassSpringSystemSimulator::simulateTimestep(float timeStep)
+{
+}
+
+void MassSpringSystemSimulator::onClick(int x, int y)
+{
+}
+
+void MassSpringSystemSimulator::onMouse(int x, int y)
+{
+}
+
+// Specific Functions
+void MassSpringSystemSimulator::setIntegrator(int integrator)
+{
+}
+
+void MassSpringSystemSimulator::setMass(float mass)
+{
+}
+
+void MassSpringSystemSimulator::setStiffness(float stiffness)
+{
+}
+
+void MassSpringSystemSimulator::setDampingFactor(float damping)
+{
+}
+
+int MassSpringSystemSimulator::addMassPoint(Vec3 position, Vec3 Velocity, bool isFixed)
+{
+	return 0;
+}
+
+void MassSpringSystemSimulator::addSpring(int masspoint1, int masspoint2, float initialLength)
+{
+}
+
+int MassSpringSystemSimulator::getNumberOfMassPoints()
+{
+	return 0;
+}
+
+int MassSpringSystemSimulator::getNumberOfSprings()
+{
+	return 0;
+}
+
+Vec3 MassSpringSystemSimulator::getPositionOfMassPoint(int index)
+{
+	return Vec3();
+}
+
+Vec3 MassSpringSystemSimulator::getVelocityOfMassPoint(int index)
+{
+	return Vec3();
+}
+
+void MassSpringSystemSimulator::applyExternalForce(Vec3 force)
+{
+}
